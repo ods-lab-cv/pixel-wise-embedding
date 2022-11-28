11c759c39af098dd89ad364b9d741576c3f6730c
Alex Rezler <ARezler-consultant@Scholastic.com>
2022-11-28 23:27:57 +0700

    feat: Altered config.py file and created run.py file. Run.py is meant to be main file executing project's pipeline.

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
 import typing as tp
+from train import BaseStep, TrainStep, ValStep
+from train import MODEL_KEY, COLLECTOR_KEY, LOSS_KEY, METRIC_KEY
+from tester import Tester
+import albumentations as A
+import torch
+import os
 
 
 @dataclass
@@ -12,3 +18,129 @@ class BaseConfig:
 
     ade20k_dataset_path: str
     device: str
+
+
+class Config:
+    previous_config_name: str = None
+
+    main_: str = '/content'
+    exp_name: str = 'class_exp8'
+    result_path: str = os.path.join('results', exp_name)
+    result_tester: str = os.path.join(result_path, 'tester')
+    result_config: str = os.path.join(result_path, 'config')
+
+    features_size: int = 2
+    n_classes: int = 3688
+    device: str = 'cuda'
+    amp: bool = True
+    shape: Tuple[int, int] = (32, 32)
+    batch_size: int = 8
+    epochs: int = 1
+    backbone: str = 'efficientnet-b0'
+    ignore_classes: tp.List[int] = [0]
+
+    train_transforms: str = A.Compose(
+        [
+        A.Compose([
+                A.OneOf([A.Resize(shape[0], shape[1]),], p=1)
+            ], p=1), 
+        A.Compose([
+            A.Flip(),
+            A.ShiftScaleRotate(),
+            A.OneOf([
+                A.HueSaturationValue(),
+                A.ColorJitter(),
+            ]),
+            A.RandomBrightnessContrast(),
+            A.RandomGamma(),
+            A.MotionBlur(),
+            ])
+        ]
+    )
+    val_transforms: str = A.Resize(shape[0], shape[1])
+    dataset_path: str = os.path.join(main_, 'ade20k/ADE20K_2021_17_01')
+
+    # Extracting and preprocessing
+    get_dataset: tp.Callable = (lambda : get_ade20k(
+        train_transforms=Config.train_transforms,
+        val_transforms=Config.val_transforms,
+        dataset_path=Config.dataset_path,
+    ))
+
+    path_to_dataset_archive: str = os.path.join(main_, 'drive/MyDrive/ade20k.zip')
+    path_to_extract_dataset_archive: str = os.path.join(main_, 'ade20k')
+
+    loss: torch.nn.Module = DiceLossWithPrepare()
+
+    model: torch.nn.Module = smp.Unet(
+        backbone,
+        classes=features_size,
+        activation=None,
+        decoder_channels=[features_size*6, features_size*5, features_size*4, features_size*3, features_size*2],
+    )
+
+    out_collector: torch.nn.Module = OutCollectorWithSingleConv(
+        n_classes=n_classes,
+        features_size=features_size,
+        ignore_classes=ignore_classes,
+    )
+
+    train_step: BaseStep = TrainStep(
+        model=model,
+        out_collector=out_collector,
+        loss=loss,
+        trainable_objects=[MODEL_KEY, COLLECTOR_KEY],
+        is_log_per_cls=False,
+        device=device,
+        amp=amp,
+    )
+
+    val_step: BaseStep = ValStep(
+        model=model,
+        out_collector=out_collector,
+        loss=loss,
+        is_log_per_cls=False,
+        device=device,
+        amp=amp,
+    )
+
+    testers: tp.List[Tester] = [
+        Tester(
+            model,
+            images_paths=sorted(glob.glob('/content/pixel-wise-embedding/data/test_images/cars/*')),
+            x=0.2, y=0.4,
+            target_b=0,
+            save_folder=os.path.join(result_tester, 'cars'),
+            transforms=A.Resize(shape[0], shape[1]),
+            threshold=0.9,
+            gif_duration=500,
+            run_every=1,
+            device=device,
+        ),
+
+        Tester(
+            model,
+            images_paths=sorted(glob.glob('/content/pixel-wise-embedding/data/test_images/cats/*')),
+            x=0.4, y=0.6,
+            target_b=1,
+            save_folder=os.path.join(result_tester, 'cats'),
+            transforms=A.Resize(shape[0], shape[1]),
+            threshold=0.9,
+            gif_duration=500,
+            run_every=1,
+            device=device,
+        ),
+
+        Tester(
+            model,
+            images_paths=sorted(glob.glob('/content/pixel-wise-embedding/data/test_images/fish/*')),
+            x=0.7, y=0.7,
+            target_b=0,
+            save_folder=os.path.join(result_tester, 'fish'),
+            transforms=A.Resize(shape[0], shape[1]),
+            threshold=0.9,
+            gif_duration=500,
+            run_every=1,
+            device=device,
+        ),
+    ]
---
+++ run.py
@@ -0,0 +1,69 @@
+import gzip
+import pickle
+
+RESULT_PATH_MODEL = os.path.join(Config.result_path, 'models')
+BEST_MODEL = os.path.join(RESULT_PATH_MODEL, 'best.pth')
+LAST_MODEL = os.path.join(RESULT_PATH_MODEL, 'last.pth')
+
+os.makedirs(Config.result_path, exist_ok=True)
+os.makedirs(Config.result_tester, exist_ok=True)
+os.makedirs(RESULT_PATH_MODEL, exist_ok=True)
+
+if Config.previous_config_name is not None:
+    config_filename = os.path.join(Config.result_path, Config.previous_config_name)
+    with gzip.open(config_filename, 'rb') as f:
+        Config = pickle.load(f)
+else:
+	config_filename = os.path.join(Config.result_path, 'config.pickle')
+	with gzip.open(config_filename, 'wb') as f:
+	    pickle.dump(Config, f)
+
+train_dataset_ade20k, val_dataset_ade20k = Config.get_dataset()
+
+# https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+# DataLoader wraps an iterable around the Dataset to enable easy access to the samples
+
+train_loader_ade20k = DataLoader(train_dataset_ade20k, batch_size=Config.batch_size, shuffle=True, num_workers=6)
+val_loader_ade20k = DataLoader(val_dataset_ade20k, batch_size=Config.batch_size, shuffle=False, num_workers=6)
+
+model = Config.model
+loss = Config.loss
+out_collector = Config.out_collector
+
+train_step = Config.train_step
+val_step = Config.val_step
+
+testers = Config.testers
+
+model.to(Config.device)
+loss.to(Config.device)
+out_collector.to(Config.device)
+
+model = model.train()
+
+last_metric = None
+for epoch in range(Config.epochs):
+
+    train_logs_ade20k, train_logs_ade20k_per_cls = train_step.run(train_loader_ade20k, [])
+
+    val_logs_ade20k, val_logs_ade20k_per_cls = val_step.run(val_loader_ade20k)
+
+    cur_metric = val_logs_ade20k['loss']
+
+    torch.save(model.state_dict(), LAST_MODEL)
+    torch.save(loss.state_dict(), LAST_MODEL.replace('.pth', '_loss.pth'))
+    torch.save(out_collector.state_dict(), LAST_MODEL.replace('.pth', '_collect.pth'))
+    if last_metric is not None:
+        if cur_metric < last_metric:
+            torch.save(model.state_dict(), BEST_MODEL)
+            torch.save(loss.state_dict(), BEST_MODEL.replace('.pth', '_loss.pth'))
+            torch.save(out_collector.state_dict(), BEST_MODEL.replace('.pth', '_collect.pth'))
+    else:
+        last_metric = cur_metric
+        
+    for tester in testers:
+        tester.test()
+    
+    
+    print('train_logs_ade20k =', train_logs_ade20k)
+    print('val_logs_ade20k =', val_logs_ade20k)
